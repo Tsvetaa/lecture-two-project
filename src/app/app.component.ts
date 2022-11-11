8cfa2624362db2da191acd5a6d4898b61e388495
petkots <tsveta.v.petkova@gmail.com>
2022-11-11 15:03:04 +0100

    adding css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lecture-two-project';
+  title = 'Demo Component Lecture';
 
   changeTitleHandler(newTitle: string) {
     this.title = newTitle;
